e3075d70b1a0094b151f984833e090533a09a371
Kamil Kozik <unknown>
2019-11-06 21:23:21 +0100

    Updates - o365 init method - gdrive for duplicate file names

--- outlook/auth.py
+++ outlook/auth.py
@@ -1,5 +1,5 @@
 import os
-from typing import Callable
+from typing import Callable, List
 
 from O365 import MSOffice365Protocol, Account, FileSystemTokenBackend
 
@@ -10,13 +10,14 @@ from outlook.utils import o365_credentials
 def init_o365(
         protocol: MSOffice365Protocol = MSOffice365Protocol(),
         credentials: Callable = o365_credentials,
-        token_path: str = O365_DEFAULT_TOKEN_PATH
+        token_path: str = O365_DEFAULT_TOKEN_PATH,
+        scopes: List[str] = O365_DEFAULT_SCOPES
 ):
-    token_backend = FileSystemTokenBackend(token_path=token_path)
+    token_backend = FileSystemTokenBackend(token_path=os.path.join(token_path, O365_DEFAULT_TOKEN_FILE_NAME))
     account = Account(credentials(), protocol=protocol, token_backend=token_backend)
 
     if not account.is_authenticated and os.path.exists(os.path.join(token_path, O365_DEFAULT_TOKEN_FILE_NAME)):
         account.con.refresh_token()
     elif not account.is_authenticated:
-        account.authenticate(scopes=O365_DEFAULT_SCOPES, grant_type='client_credentials')
+        account.authenticate(scopes=scopes, grant_type='client_credentials')
     return account
